c87b502b5d127e59d708f63630c7901cf7969112
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-25 20:44:03 +0900

    wip

--- src/infrastructure/query/GA4PVQueryAdapter.ts
+++ src/infrastructure/query/GA4PVQueryAdapter.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import { Effect, pipe } from "effect";
 import type { GA4Data } from "../../domain/GA4.js";
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 import { AnalyticsAdminServiceClient } from "@google-analytics/admin";
@@ -17,29 +17,79 @@ export class Ga4PVQueryAdapter implements PVQuery {
     });
   }
 
-  getPV(properties: string[]): Effect.Effect<GA4Data[], Error> {
+  private getProperty(id: string): Effect.Effect<string, Error> {
     return Effect.async((resume) => {
-      Promise.all(
-        properties.map(async (id) => {
-          const p = await this.admin.getProperty({ name: `properties/${id}` });
-          const r = await this.ga4.runReport({
-            property: `properties/${id}`,
-            dateRanges: [{ startDate: "yesterday", endDate: "yesterday" }],
-            metrics: [{ name: "screenPageViews" }, { name: "activeUsers" }],
-          });
-          return {
-            property: p[0].displayName ?? id,
-            pv: Number(r[0].rows?.[0]?.metricValues?.[0]?.value ?? "0"),
-            activeUsers: Number(
-              r[0].rows?.[0]?.metricValues?.[1]?.value ?? "0"
-            ),
-          };
-        })
-      )
-        .then((reports) => {
-          return resume(Effect.succeed(reports));
-        })
+      this.admin
+        .getProperty({ name: `properties/${id}` })
+        .then((p) => resume(Effect.succeed(p[0].displayName ?? id)))
         .catch((err) => resume(Effect.fail(err as Error)));
     });
+  }
+
+  private getReport(id: string): Effect.Effect<
+    {
+      property: string;
+      pv: number;
+      activeUsers: number;
+    },
+    Error
+  > {
+    return Effect.async((resume) => {
+      this.ga4
+        .runReport({
+          property: `properties/${id}`,
+          dateRanges: [{ startDate: "yesterday", endDate: "yesterday" }],
+          metrics: [{ name: "screenPageViews" }, { name: "activeUsers" }],
+        })
+        .then((p) => {
+          if (
+            p[0]?.rows === undefined ||
+            p[0]?.rows === null ||
+            p[0]?.rows?.length === 0
+          ) {
+            return resume(
+              Effect.fail(new Error("No data returned. for id: " + id))
+            );
+          }
+          const row = p[0].rows[0];
+          if (
+            row?.metricValues === undefined ||
+            row?.metricValues === null ||
+            row?.metricValues.length === 0
+          ) {
+            return resume(
+              Effect.fail(new Error("No metric values returned. for id: " + id))
+            );
+          }
+          return resume(
+            Effect.succeed({
+              property: id,
+              pv: Number(row.metricValues[0]?.value ?? "0"),
+              activeUsers: Number(row.metricValues[1]?.value ?? "0"),
+            })
+          );
+        })
+        .catch((err) => {
+          resume(Effect.fail(err as Error));
+        });
+    });
+  }
+
+  getPV(properties: string[]): Effect.Effect<GA4Data[], Error> {
+    return Effect.all(
+      properties.map((id) =>
+        pipe(
+          this.getProperty(id),
+          Effect.flatMap((propertyName) => {
+            return this.getReport(id).pipe(
+              Effect.map((report) => ({
+                ...report,
+                property: propertyName,
+              }))
+            );
+          })
+        )
+      )
+    );
   }
 }
