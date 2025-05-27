65ecc1eaed3f9213f0e1a8e0d0aae097ef2403fc
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2025-05-27 09:49:42 +0900

    Use effect config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { Effect, pipe } from "effect";
+import { Config, Effect, pipe } from "effect";
 import { GA4PVCollector } from "./application/GA4PVCollector.js";
 import { TYPES } from "./config/Types.js";
 import { container } from "./config/Container.js";
 
+function propeties(): Config.Config<string[]> {
+  return pipe(
+    Config.string("GA_PROPERTIES"),
+    Config.map((v) => v.split(",").map((s) => s.trim()))
+  );
+}
+
 async function main(): Promise<void> {
-  const propertyIds = process.env.GA_PROPERTIES?.split(",") || [];
   const collector = container.get<GA4PVCollector>(TYPES.GA4PVCollector);
-  await pipe(collector.collectAndNotifyPV(propertyIds), Effect.runPromise);
+  await pipe(
+    propeties(),
+    Effect.flatMap((propertyIds) => collector.collectAndNotifyPV(propertyIds)),
+    Effect.runPromise
+  );
 }
 
 main();
